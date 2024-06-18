6694a478e48904e50b92a7525126baa6b9f4eb68
sweeetpotaooo <sehyun5004@naver.com>
2024-06-19 02:00:52 +0900

    register

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { UserModule } from './res/user/user.module';
 import { PostModule } from './res/post/post.module';
 import { LoggerMiddleware } from './middlewares/logger.Middleware';
 import { AuthModule } from './auth/auth.module';
-import { RegisterModule } from './register/register.module';
+import { RegisterModule } from './res/register/register.module';
 
 @Module({
   imports: [UserModule, PostModule, AuthModule, RegisterModule],
