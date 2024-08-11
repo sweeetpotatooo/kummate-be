9e7b10ff10cfb39dd486dcc7cedd15c7d1e7036f
sweeetpotaooo <sehyun5004@naver.com>
2024-08-11 23:05:18 +0900

    0811

--- src/res/register/dto/sign-up-request-form.dto.ts
+++ src/res/register/dto/sign-up-request-form.dto.ts
@@ -1,7 +1,16 @@
-export class SignUpRequestForm {
-  email: string;
-  password: string;
-  nickname: string;
+import { IsEmail, IsString, MinLength, MaxLength } from 'class-validator';
 
-  // 필요한 검증 로직을 추가할 수 있습니다.
+export class SignUpRequestForm {
+  @IsEmail({}, { message: '유효한 이메일 주소를 입력하세요.' })
+  email: string;
+
+  @IsString()
+  @MinLength(8, { message: '비밀번호는 최소 8자 이상이어야 합니다.' })
+  @MaxLength(20, { message: '비밀번호는 최대 20자 이하이어야 합니다.' })
+  password: string;
+
+  @IsString()
+  @MinLength(2, { message: '닉네임은 최소 2자 이상이어야 합니다.' })
+  @MaxLength(10, { message: '닉네임은 최대 10자 이하이어야 합니다.' })
+  nickname: string;
 }
