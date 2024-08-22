8d68663522db7e79c0ede735aa21d70ecb81e950
sweeetpotaooo <sehyun5004@naver.com>
2024-08-22 23:23:06 +0900

    0824

--- src/res/user/user.controller.ts
+++ src/res/user/user.controller.ts
@@ -12,17 +12,22 @@ import { CreateUserDto } from './dto/create-user.dto';
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-  @Get()
+
+  // 기본 사용자 메인 페이지 반환
+  @Get('main')
   async getUserMainPage(): Promise<string> {
     const res = await this.userService.getUserMainPage();
-
     return res;
   }
-  @Get()
+
+  // 모든 사용자 정보 가져오기 시도, 금지 예외 발생
+  @Get('all')
   async findAll() {
     throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
   }
-  @Post()
+
+  // 회원가입 엔드포인트
+  @Post('signup')
   async postJoin(@Body() createUserDto: CreateUserDto) {
     return await this.userService.createUser(createUserDto);
   }
