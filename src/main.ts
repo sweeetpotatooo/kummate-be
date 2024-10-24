5949bb18f8c2867702f12623c75c952f0a72fb65
sweeetpotaooo <sehyun5004@naver.com>
2024-10-25 06:07:16 +0900

    1025

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
 
   // CORS 설정
   app.enableCors({
-    origin: 'http://34.64.166.13', // 리액트 애플리케이션이 동작하는 주소
+    origin: 'http://kummates.com', // 리액트 애플리케이션이 동작하는 주소
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
